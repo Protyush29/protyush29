dc2632604fb59799bac641efb686d7c63b795d90
Protyush <protyushacs@gmail.com>
2021-03-08 07:09:31 +0100

    auto launch html added

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, json
+import os
+import webbrowser
 import numpy as np
 import numexpr as ne
 
@@ -18,5 +20,14 @@ def calculator():
             data = ':-p trick questions!'
         return json.dumps({'result': data})
 
+def main():
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open_new('http://127.0.0.1:2000/')
+
+    app.run(host="127.0.0.1", port=2000)
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
